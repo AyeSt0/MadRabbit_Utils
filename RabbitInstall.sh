cff84141b5e05436408f342b555a3fd518ab5c14
AyeSt0 <66336581+AyeSt0@users.noreply.github.com>
2022-09-25 01:56:56 +0800

    Update RabbitInstall.sh

--- RabbitInstall.sh
+++ RabbitInstall.sh
@@ -18,11 +18,11 @@ function system_Judgment (){
     syosNames='synology'
     if [[ $uNames =~ $syosNames ]];then
       echo -e "\033[32m 忙猜你是群晖\033[0m"
-      echo -e "\033[42;37m 群晖一键安装脚本 \033[0m 启动>>>>>>>>>>"
+      echo -e "\033[42;37m 群晖一键安装脚本!!!启动>>>>>>>>>>>>\033[0m "
       Synology_menu
     else
       echo -e "\033[32m 忙猜你是云服务器\033[0m"
-      echo -e "\033[42;37m 云服务器一键安装脚本 \033[0m  启动>>>>>>>>>>"
+      echo -e "\033[42;37m 云服务器一键安装脚本!!!启动>>>>>>>>>>>>\033[0m"
       Cloud_menu
     fi
 }
