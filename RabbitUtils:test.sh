a57d0e1450434780363fe4e8ff19b71fb03c7e30
AyeSt0 <66336581+AyeSt0@users.noreply.github.com>
2022-11-30 15:55:07 +0800

    预更新，增加Rabbit多版本判断，提前适配FastRabbit、MadRabbit以及Rabbit

--- RabbitUtils:test.sh
+++ RabbitUtils:test.sh
@@ -7,12 +7,12 @@ DATE:2022-11-30
 DESCRIBE:One click installation of rabbit
 SYSTEM:linux
 WARNING:This script is only used for testing, learning and research. It is not allowed to be used for commercial purposes. Its legitimacy, accuracy, integrity and effectiveness cannot be guaranteed. Please make your own judgment according to the situation. The original author's warehouse address is https://github.com/HT944/MadRabbit
-VERSION:1.2.0
+VERSION:1.1.6
 MODIFY:debug
 INFO
 clear
 trap "" 2 3 15
-vVersion='T1.2.0'
+vVersion='1.1.6'
 uUser=$(whoami)
 dDate=$(date +%d/%m/%Y)
 function system_Judgment() {
@@ -2276,7 +2276,7 @@ function container_install_gn() {
         #检测更新倒计时
         Upgrade_Countdown
         #更新检测
-        update
+        update_$RabbitImageName
     fi
 }
 
@@ -2386,7 +2386,7 @@ function container_install_sy() {
         #检测更新倒计时
         Upgrade_Countdown
         #更新检测
-        update
+        update_$RabbitImageName
     fi
 }
 #是否国内
